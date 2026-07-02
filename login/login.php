6407b7ebca92d8c896f826293ff31423eae8bbca
jonathan <ojonmarqs@gmail.com>
2026-07-01 21:47:48 -0300

    arquivos passados para PHP

--- login/login.php
+++ login/login.php
@@ -8,7 +8,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 
-  <link rel="stylesheet" href="./FinMap_Projeto/css/login.css">
+  <link rel="stylesheet" href="../assets/css/login.css">
 </head>
 <body>
 
@@ -68,7 +68,7 @@
 
         <div class="register-redirect text-center mt-4">
           <span>Ainda não tem uma conta?</span>
-          <a href="registro.html" class="register-link">Criar conta</a>
+          <a href="registro.php" class="register-link">Criar conta</a>
         </div>
 
       </form>
@@ -91,7 +91,7 @@
 
     document.getElementById("loginForm").addEventListener("submit", function(e) {
       e.preventDefault();
-      window.location.href = ""; //COLOCAR TELA DE DASHBOARD AQUI
+      window.location.href = "../pages/dashboard.php";
     });
   </script>
 
