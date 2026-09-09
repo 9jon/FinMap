df94eae26e18d2307888195048689e7e01f7f7df
leo <leo.prado.140808@gmail.com>
2026-09-09 19:55:12 -0300

    .

---
+++ login/config/email.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'host' => 'smtp.gmail.com',
+    'port' => 587,
+
+    'username' => 'leo.prado.140808@gmail.com',
+    'password' => 'kwsdhoumbelzkica',
+
+    'from_email' => 'leo.prado.140808@gmail.com',
+    'from_name' => 'FinMap',
+];
